e52d03bba54b805621cdc5e644833fec5c102072
iftekher <iftekher.chowdhury@gmail.com>
2019-08-01 19:07:21 -0400

    Talent typescript object, file upload & wizard

---
+++ src/app/talent.ts
@@ -0,0 +1,66 @@
+export class Talent {
+
+	id: string;
+	name: string; 
+	phoneNumber: string;
+    emailAddress: string;
+    address: string;
+
+	calendlyUrl: string;
+
+	//private TimezoneEnum timezone;
+
+	ssnNumber: string;
+
+	bankAccount: number;
+
+	routingNumber: number;
+
+
+	//private Citizenship citizenship;
+
+
+	//private Immigration immigrationStatus;
+
+
+	//private String immigrationExpiaryStr;
+
+
+	//private Date immigrationExpiary;
+
+
+	//private Degree accademicDegree;
+
+	degreeSubject: string;
+
+
+	//private Date graduationDate;
+
+
+	//private String graduationDateStr;
+
+	//private Support suuportNeeded;
+
+	//private Objective objective;
+
+
+	//private EmploymentStatus employmentStat;
+
+	salaryStart: number;
+
+	currentJob: string;
+
+	currentEmployer: string;
+
+
+	//private Date jobStartdate;
+
+
+	//private String jobStartdateStr;
+
+	jobSalary: number;
+
+	newEmployer: string;
+
+	newPosition: string;
+}
